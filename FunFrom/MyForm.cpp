e9a8ffae54d49c886f586a6cacaf51bcd4c845c2
Lu <xiaozhishang1108@outlook.com>
2018-09-01 23:48:42 +0800

    test
    test

---
+++ FunFrom/MyForm.cpp
@@ -0,0 +1,41 @@
+﻿#include "MyForm.h"
+using namespace FunFrom;
+[STAThread]
+
+int main() {
+	Application::EnableVisualStyles();
+	Application::Run(gcnew MyForm());
+	//textBox5.LostFocus += new EventHandler(txt_LostFocus);
+}
+
+void MyForm::txt_LostFocus(System::Object^  sender, System::EventArgs^  e)
+{
+	//txtInput(textBox5);
+	this->textBox1->Enabled = true;
+}
+
+void MyForm::txt_GotFocus(System::Object^  sender, System::EventArgs^  e)
+{
+	//txtInput(textBox5);
+	this->textBox1->Enabled = false;
+}
+
+Void MyForm::textBox1_MouseEnter(System::Object^  sender, System::EventArgs^  e)
+{
+	this->textBox1->Enabled = false;
+}
+
+Void MyForm::textBox1_MouseLeave(System::Object^  sender, System::EventArgs^  e)
+{
+	this->textBox1->Enabled = true;
+}
+
+Void MyForm::button1_MouseEnter(System::Object^  sender, System::EventArgs^  e)
+{
+	Random^ strRandom = gcnew Random();
+	int demo1 = strRandom->Next(500);
+	int demo2 = strRandom->Next(500);
+
+	//std::cout << "strRandom" << demo1 << "," << demo2 << std::endl;
+	this->button1->Location = System::Drawing::Point(demo1, demo2);
+}
---
+++ FunFrom/MyForm.h
@@ -0,0 +1,94 @@
+﻿#pragma once
+#include <iostream>
+namespace FunFrom {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// MyForm 摘要
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+		}
+
+	protected:
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  button1;
+	private:
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+		void MyForm::txt_LostFocus(System::Object^  sender, System::EventArgs^  e);
+		void MyForm::txt_GotFocus(System::Object^  sender, System::EventArgs^  e);
+		Void MyForm::textBox1_MouseEnter(System::Object^  sender, System::EventArgs^  e);
+		Void MyForm::textBox1_MouseLeave(System::Object^  sender, System::EventArgs^  e);
+		Void MyForm::button1_MouseEnter(System::Object^  sender, System::EventArgs^  e);
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(65, 90);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(394, 25);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->MouseEnter += gcnew System::EventHandler(this, &MyForm::textBox1_MouseEnter);
+			this->textBox1->MouseLeave += gcnew System::EventHandler(this, &MyForm::textBox1_MouseLeave);
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"宋体", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->button1->Location = System::Drawing::Point(363, 197);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(133, 36);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"点到我算我输";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->MouseEnter += gcnew System::EventHandler(this, &MyForm::button1_MouseEnter);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(921, 647);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+
+	};
+}
